c5929356274e060059d717f313e9171f7d72ecd6
engag1ng <131041909+engag1ng@users.noreply.github.com>
2025-06-28 09:47:31 +1000

    Added config.json for remembering settings

--- app.py
+++ app.py
@@ -2,12 +2,30 @@ from flask import Flask, render_template, request
 from backend.indexer import *
 from backend.search import search_index
 import webbrowser
+import json
+import os
+
+SETTINGS_FILE = 'config.json'
+
+def load_settings():
+    if os.path.exists(SETTINGS_FILE):
+        with open(SETTINGS_FILE, 'r') as f:
+            return json.load(f)
+    return {"recursive": False, "replace_filename": False}
+
+def save_settings(settings):
+    with open(SETTINGS_FILE, 'w') as f:
+        json.dump(settings, f)
+
+def render_index(i=None):
+    settings = load_settings()
+    return render_template('index.html', i=i, settings=settings)
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', i=None)
+    return render_index()
 
 @app.route('/indexing', methods=['POST'])
 def indexing():
@@ -15,13 +33,16 @@ def indexing():
     recursive = 'recursive' in request.form
     replace_filename = 'replace_filename' in request.form
 
+    save_settings({
+        "recursive": recursive,
+        "replace_filename": replace_filename
+    })
+
     i, without_id = get_files_without_id(path, recursive)
-
     with_id = assign_id(replace_filename, without_id)
-
     index_files(with_id)
 
-    return render_template('index.html', i=i)
+    return render_index(i=i)
 
 @app.route('/search', methods=['GET'])
 def search():
